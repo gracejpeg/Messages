fc3129a2a5f4c306f0239641391a74d7b9795d1f
James Quinlan <jquinlan@yesware.com>
2018-11-07 22:39:36 +0100

    sync fork

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,36 +3,41 @@ chrome.extension.sendMessage({}, function(response) {
     
     var readyStateCheckInterval = setInterval(function() {
         if (document.readyState === "complete") {
+
+            console.log('Injection executed');
             clearInterval(readyStateCheckInterval);
 
+            // Load in our Google Fonts
             window.WebFont.load({
                 google: {
                     families: ['Roboto Mono', 'UnifrakturMaguntia', 'Amiri', 'Yatra One']
                 }
             });
 
-            // ----------------------------------------------------------
-            // This part of the script triggers when page is done loading
-            console.log("Hello. This message was sent from scripts/inject.js");
-            // ----------------------------------------------------------
+            var images = {
+                first: '<img src="imgur.com/grace-image"></img></div>',
+                second: '<img src="imgur.com/grace-image"></img>',
+            }
 
-            $('.fork-flag').first().text("LOOK AT THIS TEXT");
-            console.log($('.fork-flag').first())
+            var randomNumber = Math.floor(Math.random() * Math.floor(2));
+            var randomImageKey = Object.keys(images)[randomNumber]; // "first", "second", etc.
+
+            var sentences = {
+                first: '<div class="fontBlackletter fontSans fontMono fontSerif font Display">' + images[randomImageKey] + '<span class="fontBlackletter">sometimes </span><span class="fontSerif">I </span><span class="fontMono">left </span><span class="fontSans">messages </span><span class="fontSerif">in the </span><span class="fontDisplay">street.</span></div>',
+                second: '<div class="fontBlackletter fontSans fontMono fontSerif font Display"><span class="fontBlackletter">sometimes </span><span class="fontSerif">I </span><span class="fontMono">left </span><span class="fontSans">messages </span><span class="fontSerif">in the </span><span class="fontDisplay">butt.</span></div>',
+                third: '<div class="fontBlackletter fontSans fontMono fontSerif font Display"><span class="fontBlackletter">sometimes </span><span class="fontSerif">I </span><span class="fontMono">left </span><span class="fontSans">messages </span><span class="fontSerif">in the </span><span class="fontDisplay">moon.</span></div>',
+                fourth: '<div class="fontBlackletter fontSans fontMono fontSerif font Display"><span class="fontBlackletter">sometimes </span><span class="fontSerif">I </span><span class="fontMono">left </span><span class="fontSans">messages </span><span class="fontSerif">in the </span><span class="fontDisplay">Boston.</span></div>',
+            }
+
+            var randomNumber = Math.floor(Math.random() * Math.floor(4));
+            var randomKey = Object.keys(sentences)[randomNumber]; // "first", "second", etc.
 
             var popup = $('<div class="popup show"></div>');
+            var text = $(sentences[randomKey]);
 
-            /**var text = Math.random() > 0.5 ? "Text was greater than 0.5": "Text was lower than 0.5"
-            popup.text(text)*/
-
-            var text = $('<div class="fontBlackletter fontSans fontMono fontSerif font Display"><span class="fontBlackletter">sometimes </span><span class="fontSerif">I </span><span class="fontMono">left </span><span class="fontSans">messages </span><span class="fontSerif">in the </span><span class="fontDisplay">street.</span></div>');
             popup.append(text)
             $('body').append(popup);
             
-            /*var image1 = new Image();
-            image1.src = 
-            popup.append(image1)
-            $('body').append(popup);*/
-
         }
     }, 10);
 });
